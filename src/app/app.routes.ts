ec21dee804fcc46157ca031a9a94e46f902f6dc5
Artem Mishin <artem.mishinbet98@mail.ru>
2025-03-11 01:58:26 +0400

    Added app config(services), routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CreateReportComponent } from './create-report/create-report.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: CreateReportComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: ''
+  }
+];
